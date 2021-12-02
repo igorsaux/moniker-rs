78ee23f1fb2e519538dfeb43b687617e0623b346
igorsaux <igorsa.ux@outlook.com>
2021-12-02 21:18:29 +0300

    Add conditional compilation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
+#[cfg(feature = "animal")]
 mod animal;
+#[cfg(feature = "moby")]
 mod moby;
+
 mod moniker;
 mod random;
 
+#[cfg(feature = "animal")]
 pub use animal::Animal;
+
+#[cfg(feature = "moby")]
 pub use moby::Moby;
+
 pub use moniker::Moniker;
